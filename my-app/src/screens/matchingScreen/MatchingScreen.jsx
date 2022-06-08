3fcc3c4cf6e87a8a6981ba82f2e97ec59c12ed81
Jialong Lu <74952538+jialongclu@users.noreply.github.com>
2022-06-07 17:35:12 -0700

    added nav bar, matching card, matching screen

---
+++ my-app/src/screens/matchingScreen/MatchingScreen.jsx
@@ -0,0 +1,14 @@
+import MatchingCard from '../../components/MatchingCard';
+import NavBar from '../../components/NavBar';
+import './MatchingScreen.css'; 
+
+
+function MatchingScreen() {
+  return (
+    <div className="matchingScreen">
+        <MatchingCard />
+    </div>
+  );
+}
+
+export default MatchingScreen;
