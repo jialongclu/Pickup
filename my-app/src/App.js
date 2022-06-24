154eaad16504b58f2b907ef5bfaff80129193597
Tanraj Dhillon <tanrajsd@gamil.com>
2022-06-23 17:16:08 -0700

    add auth 0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MatchingScreen from './screens/matchingScreen/MatchingScreen.jsx';
-import SignUp from './components/SignUp/SignUp.jsx'
-import SignIn from "./components/SignIn/SignIn.jsx";
-import "./App.css"
+import SignUp from './components/SignUp/SignUp.jsx';
+import SignIn from './components/SignIn/SignIn.jsx';
+import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <>
-    <NavBar />
-    <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/matchingScreen" element={<MatchingScreen />} />
-            <Route path="/signUp" element={<SignUp />}/>
-            <Route path="/signIn" element={<SignIn />}/>
-          </Routes>
-        </Router>
-    </div>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <div className="App">
+                <Router>
+                    <Routes>
+                        <Route
+                            path="/matchingScreen"
+                            element={<MatchingScreen />}
+                        />
+                        <Route path="/signUp" element={<SignUp />} />
+                        <Route path="/signIn" element={<SignIn />} />
+                    </Routes>
+                </Router>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Auth0ProviderWithHistory from '../au';
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const client = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <Router>
+        <Auth0ProviderWithHistory>
+            <App />
+        </Auth0ProviderWithHistory>
+    </Router>
 );
