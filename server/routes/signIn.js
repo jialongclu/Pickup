1fd6b4c0339f8b9c01a8926fa5f9cf45a299a026
Jialong Lu <74952538+jialongclu@users.noreply.github.com>
2022-07-29 07:55:06 -0700

    fix repo struct

---
+++ server/routes/signIn.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const { checkPassword } = require('../../client/src/database/index')
+
+router.post('/', function (req, res, next) {
+    const { email, password } = req.body;
+    checkPassword({ email, password }).then((resp) => res.send(resp)).catch((err) => res.send(err))
+});
+
+module.exports = router;
---
+++ server/routes/signUp.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const { createUser } = require('../../client/src/database/index')
+
+router.put('/', function (req, res, next) {
+    const userData = req.body;
+    createUser(userData).then((resp) => res.sendStatus(200)).catch((err) => res.send(err))
+});
+
+module.exports = router;
