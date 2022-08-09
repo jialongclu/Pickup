0ee97fbc3190066adeb11ba1fe3fdbe93171436d
Jialong Lu <74952538+jialongclu@users.noreply.github.com>
2022-08-09 14:43:03 -0700

    logout goes to homepage

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -23,9 +23,9 @@ const NavBar = () => {
     { name: "Profile", link: <Link to="/EditProfile">Profile</Link> },
     { name: "Matches", link: <Link to="/userMatches">Matches</Link> },
     { name: "Keep swiping", link: <Link to="/matchingScreen">Keep Swiping</Link> },
-    { name: "Logout", link: <Link to="/signIn" onClick={() => {
+    { name: "Logout", link: <Link to="/homePage" onClick={() => {
       dispatch({type: actionTypes.LOGOUT})
-      navigate("/signIn")
+      navigate("/homePage")
     }}>Logout</Link>}
   ];
 
