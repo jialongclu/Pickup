61feeb115d64bdbc59d4a31494f725bf77978c73
Jialong Lu <74952538+jialongclu@users.noreply.github.com>
2022-08-09 13:15:54 -0700

    navBar has profile picture

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -63,7 +63,7 @@ const NavBar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Jia" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Jia" src={currentUser.image} />
               </IconButton>
             </Tooltip>
             <Menu
